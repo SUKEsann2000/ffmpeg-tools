8f260193e26e1a125ef06755a7d94beedffcb617
SUKEsann2000 <tyounan.i.sub@gmail.com>
2024-08-23 12:14:31 +0900

    modifired install.bat

--- install.bat
+++ install.bat
@@ -12,7 +12,7 @@ IF %yn_check:Y=Y%==Y (
 	rem レジストリ変更するなら
 	reg add "HKEY_CLASSES_ROOT\*\shell\video2mp3" /ve /d "videoのファイルをmp3に変換"
 	echo %~dp0"video-2-mp3.bat %%1"
-	reg add "HKEY_CLASSES_ROOT\*\shell\video2mp3\command" /ve /d "%~dp0video-2-mp3.bat %%1"
+	reg add "HKEY_CLASSES_ROOT\*\shell\video2mp3\command" /ve /d "%~dp0video-2-mp3.bat \"%%1\""
 	pause
 ) ELSE IF %yn_check:N=N%==N  (
 	rem レジストリ変更なし
